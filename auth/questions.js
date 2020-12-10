d2e6f90bf6d4ef50339bb6c4d1393d2961a710b7
politicaltests <75597907+politicaltests@users.noreply.github.com>
2020-12-10 12:32:09 +0800

    Update questions.js

--- auth/questions.js
+++ auth/questions.js
@@ -1,21 +1,3 @@
-const rowList = [
-           // axisName,	y, 		height
-            [ "test",	1, 1, 1, 1, 1, 1, 1, 1 ],
-            [ "efe",	1, 1, 1, 1, 1, 1, 1, 1 ],
-
-
-        ] // transform the list into objects containing:
-          // axisName, y, height (from above), value (from URL)
-        .map(ary) {
-            const “question” = ary[0];
-                var "olig" = ary[1]      
-                var "civil" = ary[2]     
-                var "elit" = ary[3]      
-                var "preve" = ary[4]     
-                var "diver" = ary[5]     
-                var "ruleund" = ary[6]   
-                var "bestsuit" = ary[7]  
-                var "disun" = ary[8]
-
-            };
-        });
+  questions = [
+    { "question": "It's best if a single person has unlimited power over government.", "effect":{
+    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 10, "ruleund": 0, "bestsuit": 0, "disun": 0 }},

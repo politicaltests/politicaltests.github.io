4c0019e6d9e6f3fd687fedff11dafbf95c9faee6
politicaltests <75597907+politicaltests@users.noreply.github.com>
2020-12-10 12:11:01 +0800

    Update questions.js

--- auth/questions.js
+++ auth/questions.js
@@ -6,14 +6,14 @@ const rowList = [
           // axisName, y, height (from above), value (from URL)
         .map(ary) {
             const “question” = ary[0];
-                var olig = ary[1]
-                var civil = ary[2]
-                var elit = ary[3]
-                var preve = ary[4]
-                var diver = ary[5]
-                var ruleund = ary[6]
-                var bestsuit = ary[7]
-                var disun = ary[8]
+                var "olig" = ary[1]      
+                var "civil" = ary[2]     
+                var "elit" = ary[3]      
+                var "preve" = ary[4]     
+                var "diver" = ary[5]     
+                var "ruleund" = ary[6]   
+                var "bestsuit" = ary[7]  
+                var "disun" = ary[8]
 
             };
         });

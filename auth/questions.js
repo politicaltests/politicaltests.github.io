fd796e168a177d035ee4a8e1a8b708804f9f0658
politicaltests <75597907+politicaltests@users.noreply.github.com>
2020-12-10 12:34:06 +0800

    Update questions.js

--- auth/questions.js
+++ auth/questions.js
@@ -1,3 +1,9 @@
   questions = [
+    
     { "question": "It's best if a single person has unlimited power over government.", "effect":{
     "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 10, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+    
+    { "question": "Authoritarian Socialists and Authoritarian Capitalists are too different to work toghether.", "effect":{
+    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 10, "ruleund": 0, "bestsuit": 0, "disun": 10 }},
+    
+    ];

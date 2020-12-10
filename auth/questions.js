604c5d792b01dc435aa91ffbed4c54fb9e5f95d4
politicaltests <75597907+politicaltests@users.noreply.github.com>
2020-12-10 15:51:28 +0800

    Update questions.js

--- auth/questions.js
+++ auth/questions.js
@@ -1,9 +1,116 @@
-  questions = [
-    
+questions = [
+
+//[1] Autocracy
     { "question": "It's best if a single person has unlimited power over government.", "effect":{
-    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 10, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+    "olig": -10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+
+    { "question": "An oligarchy or democracy is ineffecient and slow.", "effect":{
+    "olig": -10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
     
+    { "question": "The revolutions within the Age of Revolution was generally disastrous towards the world as a whole.", "effect":{
+    "olig": -10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "An Absolute Monarchy is more preferable than a representative democracy.", "effect":{
+    "olig": -10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Autocracy is often suprerior to Democracy.", "effect":{
+    "olig": -10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+// [1] Oligarchy 
+    { "question": "Modern democracies are better than Absolute Monarchies.", "effect":{
+    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Although it's better than an Autocracy, oligarchies are still coercive.", "effect":{
+    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Freedom and Liberty is important to me.", "effect":{
+    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Regimes like China or North Korea are evil and/or genocidal.", "effect":{
+    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Checks-and-balances or Bureacracy are needed for a good government.", "effect":{
+    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+ //[2] Economic
+    { "question": "Corporations are inherently evil.", "effect":{
+    "olig": 0, "civil": -10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "We need more intervention in terms of the economy.", "effect":{
+    "olig": 0, "civil": -10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Socialism or Communism is better than Capitalism.", "effect":{
+    "olig": 0, "civil": -10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Most of the problems we face today can be solved with economic regulation.", "effect":{
+    "olig": 0, "civil": -10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "The current military budget of my country is too high.", "effect":{
+    "olig": 0, "civil": -10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+ //[2] Civil
+    { "question": "Law and Order makes society function properly.", "effect":{
+    "olig": 0, "civil": 10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Without a reliable police force, a society is nothing.", "effect":{
+    "olig": 0, "civil": 10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "We need to increase the efforts on our national defence.", "effect":{
+    "olig": 0, "civil": 10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Economic regulation destroys small businesses and harms society.", "effect":{
+    "olig": 0, "civil": 10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Capitalism is the only economic system that worked.", "effect":{
+    "olig": 0, "civil": 10, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+
+//[3] Populism
+    { "question": "The people can make a government function, but they can also overthrow a government.", "effect":{
+    "olig": 0, "civil": 0, "elit": -10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "The government should persuade the people, not force the people.", "effect":{
+    "olig": 0, "civil": 0, "elit": -10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "The recent rise of Left-Wing and Right-Wing Populism is mostly beneficial.", "effect":{
+    "olig": 0, "civil": 0, "elit": -10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "The government should be close to the people, these two things cannot be seperated.", "effect":{
+    "olig": 0, "civil": 0, "elit": -10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Democracy is good.", "effect":{
+    "olig": 0, "civil": 0, "elit": -10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+//[3] Elitism
+    { "question": "The ordinary citizen is too foolish to choose a path for the nation.", "effect":{
+    "olig": 0, "civil": 0, "elit": 10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "If the people are not persuaded, force and oppression is justified.", "effect":{
+    "olig": 0, "civil": 0, "elit": 10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "The ruling elite has nothing to do with the lower or middle class.", "effect":{
+    "olig": 0, "civil": 0, "elit": 10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "The Enlightenment was a mistake.", "effect":{
+    "olig": 0, "civil": 0, "elit": 10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "Citizens who don't listen to the government are often just rebellious and uncooperative people.", "effect":{
+    "olig": 0, "civil": 0, "elit": 10, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+//[4] Deterrence
+    { "question": "A person is innocent until proven guilty.", "effect":{
+    "olig": 0, "civil": 0, "elit": 0, "preve": -10, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "A person can only be punished if he is rightfully proven to have broken the law.", "effect":{
+    "olig": 0, "civil": 0, "elit": 0, "preve": -10, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "I somewhat agree with the Non-Aggression Principle.", "effect":{
+    "olig": 0, "civil": 0, "elit": 0, "preve": -10, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+    { "question": "I prefer rehabilitation over punishment.", "effect":{
+    "olig": 0, "civil": 0, "elit": 0, "preve": -10, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+
+// [8] Disunity
     { "question": "Authoritarian Socialists and Authoritarian Capitalists are too different to work toghether.", "effect":{
-    "olig": 10, "civil": 0, "elit": 0, "preve": 0, "diver": 10, "ruleund": 0, "bestsuit": 0, "disun": 10 }}
+    "olig": 0, "civil": 0, "elit": 0, "preve": 0, "diver": 10, "ruleund": 0, "bestsuit": 0, "disun": 10 }}
     
     ];

248e08ea28943788ef6422b6f212ff031d882a67
politicaltests <75597907+politicaltests@users.noreply.github.com>
2020-12-18 15:29:48 +0800

    Create ideologies.js

---
+++ auth/ideologies.js
@@ -0,0 +1,10 @@
+ideologies = [
+
+{ "name": "test",
+  "link": "https://en.wikipedia.org/wiki/Centrism",
+	"desc": "Centrism is a political position that supports a balance between left-leaning and right-leaning policies. This option could also show up if there isn't an ideology for your results. Please fill in the form if this is the case.",
+	"next": "Radical Centrism, Apoliticism, Others",
+  "stats": { "olig": -10, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 0, "bestsuit": 0, "disun": 0 }},
+  
+      
+];

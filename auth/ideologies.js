69cc9b01b6e2e933ccb40ad6a392aaf85e226e66
politicaltests <75597907+politicaltests@users.noreply.github.com>
2020-12-21 11:41:27 +0800

    Update ideologies.js

--- auth/ideologies.js
+++ auth/ideologies.js
@@ -1,10 +1,15 @@
 ideologies = [
 
-{"name": "test",
+{ "name": "test",
 "link": "https://en.wikipedia.org/wiki/Centrism",
 "desc": "Centrism is a political position that supports a balance between left-leaning and right-leaning policies. This option could also show up if there isn't an ideology for your results. Please fill in the form if this is the case.",
 "next": "Radical Centrism, Apoliticism, Others",
 "stats": { "olig": 50, "civil": 50, "elit": 50, "preve": 50, "diver": 50, "ruleund": 50, "bestsuit": 50, "disun": 50 }},
   
+{ "name": "ewewe",
+"link": "https://en.wikipedia.org/wiki/e",
+"desc": "Centrism is a political position that supports a balance between left-leaning and right-leaning policies. This option could also show up if there isn't an ideology for your results. Please fill in the form if this is the case.",
+"next": "Radical Centrism, Apoliticism, Others",
+"stats": { "olig": 50, "civil": 100, "elit": 50, "preve": 50, "diver": 50, "ruleund": 50, "bestsuit": 50, "disun": 50 }},
       
 ];

74356b08988fe291106d1cbe2215b2c0501cd754
politicaltests <75597907+politicaltests@users.noreply.github.com>
2020-12-21 15:56:11 +0800

    Update ideologies.js

--- auth/ideologies.js
+++ auth/ideologies.js
@@ -1,15 +1,58 @@
 ideologies = [
 
-{ "name": "TBD",
-"link": "https://en.wikipedia.org/wiki/TBD_(disambiguation)",
-"desc": "TBD is an abbreviation often meaning in ordinary writing 'to be discussed', 'to be done', 'to be defined', 'to be decided', 'to be determined', 'to be declared', etc. ",
-"next": "Idk",
-"stats": { "olig": 50, "civil": 50, "elit": 50, "preve": 50, "diver": 50, "ruleund": 50, "bestsuit": 50, "disun": 50 }},
+//AuthLeft
+{ "name": "Marxism-Leninism",
+"link": "https://en.wikipedia.org/wiki/Marxism%E2%80%93Leninism",
+"desc": "Marxism–Leninism is a communist ideology and the main communist movement throughout the 20th century. Marxist–Leninists support proletarian internationalism, socialist democracy, and oppose anarchism, fascism, imperialism, and liberal democracy. Marxism–Leninism holds that a two-stage communist revolution is needed to replace capitalism.",
+"next": "Leninism, Authoritarian Socialism, State Socialism",
+"stats": { "olig": 40, "civil": 10, "elit": 20, "preve": 50, "diver": 50, "ruleund": 40, "bestsuit": 60, "disun": 70 }},
+  
+{ "name": "Leninism",
+"link": "https://en.wikipedia.org/wiki/Leninism",
+"desc": "Leninism is a form of Marxism-Leninism that is developed by Russian Marxist revolutionary Vladimir Lenin that proposes the establishment of the dictatorship of the proletariat, led by a revolutionary vanguard party, as the political prelude to the establishment of communism.",
+"next": "Marxism-Leninism, Authoritarian Socialism, Stalinism",
+"stats": { "olig": 35, "civil": 10, "elit": 40, "preve": 50, "diver": 50, "ruleund": 30, "bestsuit": 70, "disun": 90 }},
+  
+{ "name": "Stalinism",
+"link": "https://en.wikipedia.org/wiki/Stalinism",
+"desc": "Stalinism refers to the ideological beliefs and policies of Joseph Stalin. It is based on Marxism–Leninism and included the creation of a one-party totalitarian police state, rapid industrialization, the theory of socialism in one country, collectivization of agriculture, and an intensification of the class struggle under socialism.",
+"next": "Marxism-Leninism, Authoritarian Socialism, State Socialism, Maoism",
+"stats": { "olig": 10, "civil": 5, "elit": 60, "preve": 50, "diver": 50, "ruleund": 10, "bestsuit": 60, "disun": 95 }},
+  
+{ "name": "Maoism",
+"link": "https://en.wikipedia.org/wiki/Maoism",
+"desc": "Maoism is a variety of Marxism–Leninism that Mao Zedong developed for realising a socialist revolution in the agricultural, pre-industrial society of the Republic of China and later the People's Republic of China. Rather than focusing on the proletariat, it focuses on the peasantry. It, like Stalinism, supports a strong state that would heavily crack down on 'Counter-Revolutionaries'.",
+"next": "Marxism-Leninism, Authoritarian Socialism, State Socialism, Maoism",
+"stats": { "olig": 15, "civil": 20, "elit": 70, "preve": 50, "diver": 50, "ruleund": 10, "bestsuit": 40, "disun": 95 }},
+  
+{ "name": "Juche",
+"link": "https://en.wikipedia.org/wiki/Juche",
+"desc": "Juche is the official ideology of North Korea, described by the government as 'Kim Il-sung's original, brilliant and revolutionary contribution to national and international thought'. ",
+"next": "Marxism-Leninism, Stalinism, Maoism",
+"stats": { "olig": 10, "civil": 20, "elit": 70, "preve": 50, "diver": 50, "ruleund": 10, "bestsuit": 15, "disun": 95 }},
+  
+{ "name": "Authoritarian Socialism",
+"link": "https://en.wikipedia.org/wiki/Authoritarian_socialism",
+"desc": "Authoritarian Socialism is a political system supporting some form of socialist economics while rejecting political liberalism. As a term, it represents a set of socialist political systems that rejects the liberal-democratic concepts of multi-party politics, freedom of assembly, and freedom of expression,  either due to fear of the counter-revolution or as a means to socialist ends.",
+"next": "Stalinism, State Socialism, Marxism-Leninism, Maoism",
+"stats": { "olig": 20, "civil": 20, "elit": 70, "preve": 50, "diver": 50, "ruleund": 15, "bestsuit": 50, "disun": 80 }},
+  
+{ "name": "State Socialism",
+"link": "https://en.wikipedia.org/wiki/State_socialism",
+"desc": "State socialism is a political and economic ideology within the socialist movement advocating state ownership of the means of production, either as a temporary measure or as a characteristic of socialism in the transition from the capitalist to the socialist mode of production or communist society.",
+"next": "Authoritarian Socialism, Marxism-Leninism, Leninism",
+"stats": { "olig": 40, "civil": 10, "elit": 50, "preve": 50, "diver": 50, "ruleund": 40, "bestsuit": 50, "disun": 70 }},
+  
+{ "name": "National Bolshevism (Nazbol)",
+"link": "https://en.wikipedia.org/wiki/National_Bolshevism",
+"desc": "National Bolshevism is a political movement and a political ideology that combines elements of fascism and Bolshevism. Being described as 'Culturally Far-Right and Economically Far-Left', it supports a socialist economy alongside with an authoritarian and ultranationalist government.",
+"next": "National Socialism, Marxism-Leninism, Leninism, Stalinism",
+"stats": { "olig": 30, "civil": 20, "elit": 40, "preve": 50, "diver": 10, "ruleund": 30, "bestsuit": 50, "disun": 10 }},
   
 { "name": "TBD",
 "link": "https://en.wikipedia.org/wiki/TBD_(disambiguation)",
 "desc": "TBD is an abbreviation often meaning in ordinary writing 'to be discussed', 'to be done', 'to be defined', 'to be decided', 'to be determined', 'to be declared', etc. ",
 "next": "Idk",
-"stats": { "olig": 100, "civil": 50, "elit": 50, "preve": 50, "diver": 50, "ruleund": 50, "bestsuit": 50, "disun": 50 }}
+"stats": { "olig": 0, "civil": 0, "elit": 0, "preve": 0, "diver": 0, "ruleund": 50, "bestsuit": 50, "disun": 50 }}
        
 ];

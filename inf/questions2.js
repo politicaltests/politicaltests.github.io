8e94b305782dbd70c9e3ce49a6870694ca0fe89f
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-02-16 17:20:13 +0800

    Update questions2.js

--- inf/questions2.js
+++ inf/questions2.js
@@ -9,9 +9,16 @@ questions =  [
     {"id": 5, "question": "Government regulation leads to the forming of large corporations.", "effects": {"cap": 1}},
     {"id": 6, "question": "Government should regulate the market as little as possible.", "effects": {"cap": 1}},
     {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"cap": 1}},
+    {"id": 8, "question": "Capitalism rose more people out of poverty than any other economic system.", "effects": {"cap": 1}},
     
  //Socialism
-     {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"cap": 1}},
+    {"id": 9, "question": "Capitalism is unjust.", "effects": {"soc": 1}},
+    {"id": 10, "question": "Healthcare should be socialized.", "effects": {"soc": 1}},
+    {"id": 11, "question": "Communal ownership is beneficial to society as a whole.", "effects": {"soc": 1}},
+    {"id": 12, "question": "I prefer democratized companies over companies using the current traditional model.", "effects": {"soc": 1}},
+    {"id": 13, "question": "Capitalism leads to oligarchy and tyranny.", "effects": {"soc": 1}},
+    {"id": 14, "question": "Workers should directly own what they produce.", "effects": {"soc": 1}},
 
-
+ //Authoritarian
+    {"id": 14, "question": "Workers should directly own what they produce.", "effects": {"soc": 1}},
  ];

2f38777addd807c9d926557ce073e73da3b58c72
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-02-16 17:06:42 +0800

    Create questions2.js

---
+++ inf/questions2.js
@@ -0,0 +1,17 @@
+questions =  [
+
+//Capitalism
+    {"id": 0, "question": "Competition often boosts innovation.", "effects": {"cap": 1}},
+    {"id": 1, "question": "Inheritance is a legitimate form of wealth.", "effects": {"cap": 1}},
+    {"id": 2, "question": "Capitalism is the most successful and beneficial economic system yet.", "effects": {"cap": 1}},
+    {"id": 3, "question": "The means of production should be owned privately.", "effects": {"cap": 1}},
+    {"id": 4, "question": "The private sector takes, the public sector takes.", "effects": {"cap": 1}},
+    {"id": 5, "question": "Government regulation leads to the forming of large corporations.", "effects": {"cap": 1}},
+    {"id": 6, "question": "Government should regulate the market as little as possible.", "effects": {"cap": 1}},
+    {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"cap": 1}},
+    
+ //Socialism
+     {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"cap": 1}},
+
+
+ ];

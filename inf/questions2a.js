c54b1906fd65b0eb4db4f660643dd4e1df072d1a
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-02-18 18:31:15 +0800

    Delete questions2a.js

--- inf/questions2a.js
+++
@@ -1,9 +0,0 @@
-{"id": 1, "question": "How would you classify yourself as?", "effects": {"nat": -1}},
-    "answers":[
-        {"label": "Authoritarian Left", "color": "#990000", 
-        {"label": "Authoritarian Left", "color": "#990000", 
-        {"label": "Authoritarian Left", "color": "#990000", 
-        {"label": "Authoritarian Left", "color": "#990000", 
-        {"label": "Authoritarian Left", "color": "#990000", 
-        ]
-    },

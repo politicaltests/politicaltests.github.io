f532aa74fa67f5d1462059e756f4e3d6d16b730e
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-02-18 21:56:39 +0800

    Update questions2.js

--- inf/questions2.js
+++ inf/questions2.js
@@ -1,23 +1,19 @@
 questions =  [
 
 //Capitalism
-    {"id": 0, "question": "Competition often boosts innovation.", "effects": {"id": 1}},
-    {"id": 1, "question": "Inheritance is a legitimate form of wealth.", "effects": {"id": 1}},
-    {"id": 4, "question": "The private sector takes, the public sector takes.", "effects": {"id": 1}},
-    {"id": 5, "question": "Government regulation leads to the forming of large corporations.", "effects": {"id": 1}},
-    {"id": 6, "question": "Government should regulate the market as little as possible.", "effects": {"id": 1}},
-    {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"id": 1}},
-    {"id": 8, "question": "Capitalism rose more people out of poverty than any other economic system.", "effects": {"id": 1}},
-    {"id": 9, "question": "Capitalism is unjust.", "effects": {"id": 1}},
-    {"id": 10, "question": "Healthcare should be socialized.", "effects": {"id": 1}},
-    {"id": 11, "question": "Communal ownership is beneficial to society as a whole.", "effects": {"id": 1}},
-    {"id": 12, "question": "Workers should directly own what they produce.", "effects": {"id": 1}},
+    {"id": 0, "question": "Competition often boosts innovation.", "effects": {"id": 10}},
+    {"id": 1, "question": "Inheritance is a legitimate form of wealth.", "effects": {"id": 10}},
+    {"id": 4, "question": "The private sector takes, the public sector takes.", "effects": {"id": 10}},
+    {"id": 5, "question": "Government regulation leads to the forming of large corporations.", "effects": {"id": 10}},
+    {"id": 6, "question": "Government should regulate the market as little as possible.", "effects": {"id": 10}},
+    {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"id": 10}},
+    {"id": 8, "question": "Capitalism rose more people out of poverty than any other economic system.", "effects": {"id": 10}},
 
  //Authoritarian
-    {"id": 15, "question": "I support a strong government.", "effects": {"id": 10}},
-    {"id": 16, "question": "Even when protesting an authoritarian government, violence cannot be accepted.", "effects": {"id": 10}},
-    {"id": 17, "question": "Security and order is more important than freedom and liberty.", "effects": {"id": 10}},
-    {"id": 18, "question": "Mandates on certain ways of life are sometimes justified.", "effects": {"id": 10}},
+    {"id": 15, "question": "I support a strong government.", "effects": {"id": 100}},
+    {"id": 16, "question": "Even when protesting an authoritarian government, violence cannot be accepted.", "effects": {"id": 100}},
+    {"id": 17, "question": "Security and order is more important than freedom and liberty.", "effects": {"id": 100}},
+    {"id": 18, "question": "Mandates on certain ways of life are sometimes justified.", "effects": {"id": 100}},
     
 
 

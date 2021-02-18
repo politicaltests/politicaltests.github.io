db33f9bc579b88cc75f609f291fd619262677441
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-02-18 16:08:26 +0800

    Create questions2a.js

---
+++ inf/questions2a.js
@@ -0,0 +1,7 @@
+{"id": 1, "question": "How would you classify yourself as?",
+    "answers":[
+        {"label": "Authoritarian Left", "color": "#990000", "effects": {"nat": -1}},
+        {"label": "Authoritarian Left", "color": "#990000", "effects": {"nat": -1}},
+        {"label": "Authoritarian Left", "color": "#990000", "effects": {"nat": -1}},
+        ]
+    },

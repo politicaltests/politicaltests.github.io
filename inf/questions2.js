31691c85bd9f52757f8ab0044b348a3b914a9da6
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-02-18 21:58:44 +0800

    Update questions2.js

--- inf/questions2.js
+++ inf/questions2.js
@@ -1,13 +1,13 @@
 questions =  [
 
 //Capitalism
-    {"id": 0, "question": "Competition often boosts innovation.", "effects": {"id": 10}},
-    {"id": 1, "question": "Inheritance is a legitimate form of wealth.", "effects": {"id": 10}},
-    {"id": 4, "question": "The private sector takes, the public sector takes.", "effects": {"id": 10}},
-    {"id": 5, "question": "Government regulation leads to the forming of large corporations.", "effects": {"id": 10}},
-    {"id": 6, "question": "Government should regulate the market as little as possible.", "effects": {"id": 10}},
-    {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"id": 10}},
-    {"id": 8, "question": "Capitalism rose more people out of poverty than any other economic system.", "effects": {"id": 10}},
+    {"id": 0, "question": "Competition often boosts innovation.", "effects": {"a": 10}},
+    {"id": 1, "question": "Inheritance is a legitimate form of wealth.", "effects": {"a": 10}},
+    {"id": 4, "question": "The private sector takes, the public sector takes.", "effects": {"a": 10}},
+    {"id": 5, "question": "Government regulation leads to the forming of large corporations.", "effects": {"a": 10}},
+    {"id": 6, "question": "Government should regulate the market as little as possible.", "effects": {"a": 10}},
+    {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"a": 10}},
+    {"id": 8, "question": "Capitalism rose more people out of poverty than any other economic system.", "effects": {"a": 10}},
 
  //Authoritarian
     {"id": 9, "question": "I support a strong government.", "effects": {"id": 100}},

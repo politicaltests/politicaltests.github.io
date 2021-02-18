85747f5d0e336f8a0b3a7a90a8818d5fa29640ef
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-02-18 21:52:55 +0800

    Update questions2.js

--- inf/questions2.js
+++ inf/questions2.js
@@ -1,34 +1,23 @@
 questions =  [
 
 //Capitalism
-    {"id": 0, "question": "Competition often boosts innovation.", "effects": {"cap": 1}},
-    {"id": 1, "question": "Inheritance is a legitimate form of wealth.", "effects": {"cap": 1}},
-    {"id": 2, "question": "Capitalism is the most successful and beneficial economic system yet.", "effects": {"cap": 1}},
-    {"id": 3, "question": "The means of production should be owned privately.", "effects": {"cap": 1}},
-    {"id": 4, "question": "The private sector takes, the public sector takes.", "effects": {"cap": 1}},
-    {"id": 5, "question": "Government regulation leads to the forming of large corporations.", "effects": {"cap": 1}},
-    {"id": 6, "question": "Government should regulate the market as little as possible.", "effects": {"cap": 1}},
-    {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"cap": 1}},
-    {"id": 8, "question": "Capitalism rose more people out of poverty than any other economic system.", "effects": {"cap": 1}},
-    
- //Socialism
-    {"id": 9, "question": "Capitalism is unjust.", "effects": {"soc": 1}},
-    {"id": 10, "question": "Healthcare should be socialized.", "effects": {"soc": 1}},
-    {"id": 11, "question": "Communal ownership is beneficial to society as a whole.", "effects": {"soc": 1}},
-    {"id": 12, "question": "I prefer democratized companies over companies using the current traditional model.", "effects": {"soc": 1}},
-    {"id": 13, "question": "Capitalism leads to oligarchy and tyranny.", "effects": {"soc": 1}},
-    {"id": 14, "question": "Workers should directly own what they produce.", "effects": {"soc": 1}},
+    {"id": 0, "question": "Competition often boosts innovation.", "effects": {"id": 1}},
+    {"id": 1, "question": "Inheritance is a legitimate form of wealth.", "effects": {"id": 1}},
+    {"id": 4, "question": "The private sector takes, the public sector takes.", "effects": {"id": 1}},
+    {"id": 5, "question": "Government regulation leads to the forming of large corporations.", "effects": {"id": 1}},
+    {"id": 6, "question": "Government should regulate the market as little as possible.", "effects": {"id": 1}},
+    {"id": 7, "question": "Private services often outperform its government counterparts.", "effects": {"id": 1}},
+    {"id": 8, "question": "Capitalism rose more people out of poverty than any other economic system.", "effects": {"id": 1}},
+    {"id": 9, "question": "Capitalism is unjust.", "effects": {"id": 1}},
+    {"id": 10, "question": "Healthcare should be socialized.", "effects": {"id": 1}},
+    {"id": 11, "question": "Communal ownership is beneficial to society as a whole.", "effects": {"id": 1}},
+    {"id": 12, "question": "Workers should directly own what they produce.", "effects": {"id": 1}},
 
  //Authoritarian
-    {"id": 15, "question": "I support a strong government.", "effects": {"auth": 1}},
-    {"id": 16, "question": "Even when protesting an authoritarian government, violence cannot be accepted.", "effects": {"auth": 1}},
-    {"id": 17, "question": "Security and order is more important than freedom and liberty.", "effects": {"auth": 1}},
-    {"id": 18, "question": "I prefer autocracy over democracy.", "effects": {"auth": 1}},
-    {"id": 19, "question": "I disagree with most of the enlightenment ideas.", "effects": {"auth": 1}},
-    {"id": 20, "question": "Mandates on certain ways of life are sometimes justified.", "effects": {"auth": 1}},
-    {"id": 21, "question": "If a certain object negatively impacts society, it should be banned.", "effects": {"auth": 1}},
-    {"id": 22, "question": "Civil disobedience must be actively suppressed.", "effects": {"auth": 1}},
-    {"id": 23, "question": "Surveillance should be used to keep society on the right track.", "effects": {"auth": 1}},
+    {"id": 15, "question": "I support a strong government.", "effects": {"id": 10}},
+    {"id": 16, "question": "Even when protesting an authoritarian government, violence cannot be accepted.", "effects": {"id": 10}},
+    {"id": 17, "question": "Security and order is more important than freedom and liberty.", "effects": {"id": 10}},
+    {"id": 18, "question": "Mandates on certain ways of life are sometimes justified.", "effects": {"id": 10}},
     
 
 

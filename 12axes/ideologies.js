160af0ea7292fbe94e0b23f119a4be45a3cce1a6
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-03-02 18:12:59 +0800

    Update ideologies.js

--- 12axes/ideologies.js
+++ 12axes/ideologies.js
@@ -4,3 +4,5 @@ ideologies = [
 "link": "https://en.wikipedia.org/wiki/Centrism",
 "desc": "Centrism is a political outlook or position that involves acceptance and/or support of a balance of social equality and a degree of social hierarchy, while opposing political changes which would result in a significant shift of society strongly to either the left or the right.",
 "stats": { "a": 50, "b": 50, "c": 50, "d": 50, "e": 50, "f": 50, "g": 50, "h": 50, "i": 50, "j": 50, "k": 50, "l": 50 }},
+
+ ]

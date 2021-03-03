81bbfd61f3f76220bd8b59500dc0df1616afadc5
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-03-03 09:09:44 +0800

    Update ideologies.js

--- 12axes/ideologies.js
+++ 12axes/ideologies.js
@@ -1,8 +1,13 @@
 ideologies = [
   
-{ "name": "Centrism",
-"link": "https://en.wikipedia.org/wiki/Centrism",
-"desc": "Centrism is a political outlook or position that involves acceptance and/or support of a balance of social equality and a degree of social hierarchy, while opposing political changes which would result in a significant shift of society strongly to either the left or the right.",
+{ "name": "Placeholder 1",
+"link": "https://en.wikipedia.org/wiki/State_socialism",
+"desc": "State socialism is a political and economic ideology within the socialist movement advocating state ownership of the means of production, either as a temporary measure or as a characteristic of socialism in the transition from the capitalist to the socialist mode of production or communist society.",
 "stats": { "a": 50, "b": 50, "c": 50, "d": 50, "e": 50, "f": 50, "g": 50, "h": 50, "i": 50, "j": 50, "k": 50, "l": 50 }},
 
- ];
+{ "name": "Placeholder 2",
+"link": "https://en.wikipedia.org/wiki/State_socialism",
+"desc": "State socialism is a political and economic ideology within the socialist movement advocating state ownership of the means of production, either as a temporary measure or as a characteristic of socialism in the transition from the capitalist to the socialist mode of production or communist society.",
+"stats": { "a": 60, "b": 50, "c": 50, "d": 50, "e": 50, "f": 50, "g": 50, "h": 50, "i": 50, "j": 50, "k": 50, "l": 50 }}
+  
+];

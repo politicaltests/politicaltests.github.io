d768f080e49e8b66bd708365237a66e2caf7b1f0
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-04-19 13:05:01 +0800

    Update ideologies.js

--- 12axes/ideologies.js
+++ 12axes/ideologies.js
@@ -214,7 +214,7 @@ ideologies = [
         }
     },
     {
-        "name": "Centrist",
+        "name": "Centrism",
         "link": "https://en.wikipedia.org/wiki/Centrism",
         "desc": "Centrism is a political outlook or position that involves acceptance and/or support of a balance of social equality and a degree of social hierarchy, while opposing political changes which would result in a significant shift of society strongly to either the left or the right.",
         "stats": {

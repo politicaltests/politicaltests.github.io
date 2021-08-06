4080f574e94a9fbec0bd1d62f414454653003240
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-06 15:10:28 +0800

    Create questions.js

---
+++ 6foundations/questions.js
@@ -0,0 +1,10 @@
+//Care
+{ "question": "The emotional suffering of others", "effect": {
+  "care": 10, "fairness": 0, "loyalty": 0, "authority": 0, "sanctity": 0, "liberty": 0 }},
+    
+{ "question": "Whether or not people are being treated differently than others", "effect": {
+  "care": 10, "fairness": 0, "loyalty": 0, "authority": 0, "sanctity": 0, "liberty": 0 }},
+    
+{ "question": "Whether or not people are being kind to each others", "effect": {
+  "care": 10, "fairness": 0, "loyalty": 0, "authority": 0, "sanctity": 0, "liberty": 0 }},
+    

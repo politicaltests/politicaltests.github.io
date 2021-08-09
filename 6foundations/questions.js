4c859977842a571358b9bb2ef341920a92309f94
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-09 14:17:48 +0800

    Update questions.js

--- 6foundations/questions.js
+++ 6foundations/questions.js
@@ -1,72 +1,72 @@
 questions = [
 //Care
 { "question": "The emotional suffering of others", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 10, "fairness": 0, "loyalty": 0 }},
     
 { "question": "Whether or not people are being treated differently than others", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 10, "fairness": 0, "loyalty": 0 }},
     
 { "question": "Whether or not people are being kind to each others", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 10, "fairness": 0, "loyalty": 0 }},
     
 { "question": "Whether or not there is social justice", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 10, "fairness": 0, "loyalty": 0 }},
     
 { "question": "Whether or not some people are socially marginalized", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 10, "fairness": 0, "loyalty": 0 }},
     
 { "question": "Whether or not systematic racism exists", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 10, "fairness": 0, "loyalty": 0 }},
     
 { "question": "Whether or not people in society are virtous", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 10, "fairness": 0, "loyalty": 0 }},
     
 { "question": "Whether or not people care for the unfortunate", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 10, "fairness": 0, "loyalty": 0 }},
     
 //Fairness
 { "question": "The level of economic inequality in the world", "effect": {
-  "care": 0, "fairness": 10, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 10, "loyalty": 0 }},
 
 { "question": "The rule of law", "effect": {
-  "care": 0, "fairness": 10, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 10, "loyalty": 0 }},
 
 { "question": "Whether or not the poor are being actively helped", "effect": {
-  "care": 0, "fairness": 10, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 10, "loyalty": 0 }},
     
 { "question": "Whether or not wealth is being distributed proportionately", "effect": {
-  "care": 0, "fairness": 10, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 10, "loyalty": 0 }},
     
 { "question": "Whether or not society is equal", "effect": {
-  "care": 0, "fairness": 10, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 10, "loyalty": 0 }},
     
 { "question": "Whether or not society is equitable", "effect": {
-  "care": 0, "fairness": 10, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 10, "loyalty": 0 }},
     
 { "question": "How inheritance causes economic inequality", "effect": {
-  "care": 0, "fairness": 10, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 10, "loyalty": 0 }},
     
 { "question": "Whether or not rich people are helping society", "effect": {
-  "care": 0, "fairness": 10, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 10, "loyalty": 0 }},
     
 //Loyalty
 { "question": "Whether or not a person showed love for their country", "effect": {
-  "care": 0, "fairness": 0, "loyalty": 10 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 0, "loyalty": 10 }},
 
 { "question": "Whether or not people are willing to sacrifice themselves for their community or country", "effect": {
-  "care": 0, "fairness": 0, "loyalty": 10 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 0, "loyalty": 10 }},
     
 { "question": "How much people are voluntarily joining the military", "effect": {
-  "care": 0, "fairness": 0, "loyalty": 10 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 0, "loyalty": 10 }},
     
 { "question": "Whether or not a person loves their country", "effect": {
-  "care": 0, "fairness": 0, "loyalty": 10 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 0, "loyalty": 10 }},
     
 { "question": "Whether or not a person kneels for the national anthem", "effect": {
-  "care": 0, "fairness": 0, "loyalty": 10 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  "care": 0, "fairness": 0, "loyalty": 10 }},
     
 { "question": "Whether or not soldiers or policemen obey their orders", "effect": {
-  "care": 0, "fairness": 0, "loyalty": 10 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }}
+  "care": 0, "fairness": 0, "loyalty": 10 }}
   
 ];
 

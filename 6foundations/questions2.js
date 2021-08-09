35ed65d1ac3b7e0f916eba5528b54fb3a1531f57
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-09 17:53:33 +0800

    Update questions2.js

--- 6foundations/questions2.js
+++ 6foundations/questions2.js
@@ -1,7 +1,62 @@
 questions = [
-//Care
-{ "question": "The emotional suffering of others", "effect": {
-  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+//Authority
+{ "question": "Authorities or hierarchies will inevitably form in every society.", "effect": {
+  "authority": 5, "sanctity": 0, "liberty": 0 }},
+  
+{ "question": "People should conform to the traditions of society.", "effect": {
+  "authority": 5, "sanctity": 0, "liberty": 0 }},
+  
+{ "question": "If I were a soldier and disagreed with my commanding officer’s orders, I would obey anyway because that is my duty.", "effect": {
+  "authority": 5, "sanctity": 0, "liberty": 0 }},
+  
+{ "question": "The most important thing for children to learn is to accept discipline.", "effect": {
+  "authority": 5, "sanctity": 0, "liberty": 0 }},
+  
+{ "question": "Men and women each have different roles to play in society.", "effect": {
+  "authority": 5, "sanctity": 0, "liberty": 0 }},
+  
+{ "question": "Without tradition, society deteriorates.", "effect": {
+  "authority": 5, "sanctity": 0, "liberty": 0 }},
+  
+  
+//Sanctity  
+{ "question": "Some acts are wrong because they are unnatural.", "effect": {
+  "authority": 0, "sanctity": 5, "liberty": 0 }},
+  
+{ "question": "<div class='tooltip'><p class='tool'>Chastity</p><span class='tooltiptext'>Self-Refraint from activity considered immoral, especially special activity</span></div> is an important virtue.", "effect": {
+  "authority": 0, "sanctity": 5, "liberty": 0 }},
+  
+{ "question": "People should not do things that are disgusting, even if no one is harmed.", "effect": {
+  "authority": 0, "sanctity": 5, "liberty": 0 }},
+  
+{ "question": "Some acts are objectively disgusting.", "effect": {
+  "authority": 0, "sanctity": 5, "liberty": 0 }},
+  
+{ "question": "I care about the standards of decency and purity.", "effect": {
+  "authority": 0, "sanctity": 5, "liberty": 0 }},
+
+{ "question": "I am concerned about whether or not someone did something disgusting.", "effect": {
+  "authority": 0, "sanctity": 5, "liberty": 0 }},
+  
+//Liberty
+
+{ "question": "Individuals should be able to do whatever they want with each other, even if it makes me uncomfortable.", "effect": {
+  "authority": 0, "sanctity": 0, "liberty": 5 }},
+  
+{ "question": "Liberty is more important than security.", "effect": {
+  "authority": 0, "sanctity": 0, "liberty": 5 }},
+  
+{ "question": "Society should emphasize on individuals, not social groups.", "effect": {
+  "authority": 0, "sanctity": 0, "liberty": 5 }},
+  
+{ "question": "Everyone has rights that are natural and inalienable.", "effect": {
+  "authority": 0, "sanctity": 0, "liberty": 5 }},  
+  
+{ "question": "Conscription is evil.", "effect": {
+  "authority": 0, "sanctity": 0, "liberty": 5 }},
+  
+{ "question": "Showing respect to an unjust authority is a foolish act.", "effect": {
+  "authority": 0, "sanctity": 0, "liberty": 5 }},
   
 ];
 

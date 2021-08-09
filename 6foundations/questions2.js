99981032d90d756124d3447dfe6e5f36c6020e7e
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-09 14:16:15 +0800

    Create questions2.js

---
+++ 6foundations/questions2.js
@@ -0,0 +1,8 @@
+questions = [
+//Care
+{ "question": "The emotional suffering of others", "effect": {
+  "care": 10, "fairness": 0, "loyalty": 0 /*, "authority": 0, "sanctity": 0, "liberty": 0 */ }},
+  
+];
+
+//Update this: https://github.com/politicaltests/politicaltests.github.io/blob/main/6foundations/quiz.html

858e512453b228d66d3f260416e68bfb6063c3cf
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-10 18:14:52 +0800

    Update ideologies.js

--- 6foundations/ideologies.js
+++ 6foundations/ideologies.js
@@ -4,3 +4,5 @@ ideologies = [
 { "name": "<a href='https://en.wikipedia.org/wiki/Fascism'> Fascism </a>",
   "desc": "Hello",
 "stats": { "care": 20, "fairness": 85, "loyalty": 90, "authority": 10, "sanctity": 50, "liberty": 50 }},
+
+]

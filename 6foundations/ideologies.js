89d0cf649403c1df4d2df3fafac64ea75763c4ad
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-10 18:14:33 +0800

    Create ideologies.js

---
+++ 6foundations/ideologies.js
@@ -0,0 +1,6 @@
+ideologies = [
+
+//Totalitarianism
+{ "name": "<a href='https://en.wikipedia.org/wiki/Fascism'> Fascism </a>",
+  "desc": "Hello",
+"stats": { "care": 20, "fairness": 85, "loyalty": 90, "authority": 10, "sanctity": 50, "liberty": 50 }},

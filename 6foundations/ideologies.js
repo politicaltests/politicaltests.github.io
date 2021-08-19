65cbecc68839dc79a925c4284b25ed9b1334e4ac
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-19 17:43:07 +0800

    Update ideologies.js

--- 6foundations/ideologies.js
+++ 6foundations/ideologies.js
@@ -1,9 +1,9 @@
 ideologies = [
 
 //Totalitarianism
-{ "name": "Fascism",
+{ "name": "Progressivism",
  "link": "https://www.reddit.com/",
- "desc": "Hello",
+ "desc": "Progressivism is a political philosophy in support of social reform.arguing that progress is being stifled by vast economic inequality between the rich and the poor;"
  "stats": { "care": 20, "fairness": 85, "loyalty": 90, "authority": 10, "sanctity": 50, "liberty": 50 }},
 
 ];

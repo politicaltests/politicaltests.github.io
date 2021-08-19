1742f6da36130cab321d68cf152572518647c008
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-19 18:43:25 +0800

    Update ideologies.js

--- 6foundations/ideologies.js
+++ 6foundations/ideologies.js
@@ -1,14 +1,19 @@
 ideologies = [
 
 //Center-Left
-{ "name": "Progressivism",
+ { "name": "Progressivism",
  "link": "https://en.wikipedia.org/wiki/Progressivism",
  "desc": "Progressivism is a political philosophy in support of social reform. It argues that progress is being stifled by vast economic inequality and violent conflict between the privileged and unprivileged. It stresses the need for measures that addresses these problems.",
  "stats": { "care": 75, "fairness": 75, "loyalty": 42, "authority": 40, "sanctity": 25, "liberty": 62 }},
  
- { "name": "Progressivism",
- "link": "https://en.wikipedia.org/wiki/Progressivism",
- "desc": "Progressivism is a political philosophy in support of social reform. It argues that progress is being stifled by vast economic inequality and violent conflict between the privileged and unprivileged. It stresses the need for measures that addresses these problems.",
- "stats": { "care": 75, "fairness": 75, "loyalty": 42, "authority": 40, "sanctity": 25, "liberty": 62 }}
+ { "name": "Conservatism",
+ "link": "https://en.wikipedia.org/wiki/Conservatism",
+ "desc": "Conservatism is a cultural, social, and political philosophy which seeks to promote and to preserve traditional social institutions. n Western culture, conservatives seek to preserve a range of institutions such as organized religion, parliamentary government, and property rights.",
+ "stats": { "care": 60, "fairness": 60, "loyalty": 62, "authority": 65, "sanctity": 60, "liberty": 75 }},
+ 
+ { "name": "Right-Libertarianism",
+ "link": "https://en.wikipedia.org/wiki/Conservatism",
+ "desc": "Right-Libertarianism is a political philosophy and movement that upholds liberty as a core principle. Right-Libertarians seek to maximize autonomy and political freedom, emphasizing free association, free markets, individualism and voluntary association.",
+ "stats": { "care": 55, "fairness": 62, "loyalty": 43, "authority": 42, "sanctity": 30, "liberty": 85 }},
 
 ];

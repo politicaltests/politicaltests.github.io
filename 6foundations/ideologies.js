24f759bf27e6b3fce3b6416d3c518e3c176ee53b
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-19 18:04:01 +0800

    Update ideologies.js

--- 6foundations/ideologies.js
+++ 6foundations/ideologies.js
@@ -3,12 +3,12 @@ ideologies = [
 //Center-Left
 { "name": "Progressivism",
  "link": "https://en.wikipedia.org/wiki/Progressivism",
- "desc": "Progressivism is a political philosophy in support of social reform. It argues that progress is being stifled by vast economic inequality and violent conflict between the privileged and unprivileged. It stresses the need for measures that addresses these problems."
+ "desc": "Progressivism is a political philosophy in support of social reform. It argues that progress is being stifled by vast economic inequality and violent conflict between the privileged and unprivileged. It stresses the need for measures that addresses these problems.",
  "stats": { "care": 75, "fairness": 75, "loyalty": 42, "authority": 40, "sanctity": 25, "liberty": 62 }},
  
  { "name": "Progressivism",
  "link": "https://en.wikipedia.org/wiki/Progressivism",
- "desc": "Progressivism is a political philosophy in support of social reform. It argues that progress is being stifled by vast economic inequality and violent conflict between the privileged and unprivileged. It stresses the need for measures that addresses these problems."
+ "desc": "Progressivism is a political philosophy in support of social reform. It argues that progress is being stifled by vast economic inequality and violent conflict between the privileged and unprivileged. It stresses the need for measures that addresses these problems.",
  "stats": { "care": 75, "fairness": 75, "loyalty": 42, "authority": 40, "sanctity": 25, "liberty": 62 }}
 
 ];

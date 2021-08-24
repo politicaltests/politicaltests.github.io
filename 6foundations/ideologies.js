d522f70b4396e34d1fba569e50237c20e6efb923
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-24 22:21:06 +0800

    Update ideologies.js

--- 6foundations/ideologies.js
+++ 6foundations/ideologies.js
@@ -1,39 +1,40 @@
 ideologies = [
-
+ //Care - Progressives
  { "name": "Progressivism",
  "link": "https://en.wikipedia.org/wiki/Progressivism",
  "desc": "Progressivism is a political philosophy in support of social reform. It argues that progress is being stifled by vast economic inequality and violent conflict between the privileged and unprivileged. It stresses the need for measures that addresses these problems.",
- "next": "text",
+ "next": "Social Democracy, Left-Wing Populism, Liberalism",
  "stats": { "care": 75, "fairness": 75, "loyalty": 42, "authority": 40, "sanctity": 25, "liberty": 62 }},
  
  { "name": "Conservatism",
  "link": "https://en.wikipedia.org/wiki/Conservatism",
  "desc": "Conservatism is a cultural, social, and political philosophy which seeks to promote and to preserve traditional social institutions. n Western culture, conservatives seek to preserve a range of institutions such as organized religion, parliamentary government, and property rights.",
- "next": "text",
+ "next": "Neoconservatism, Right-Wing Populism, Fiscal Conservatism",
  "stats": { "care": 60, "fairness": 60, "loyalty": 62, "authority": 65, "sanctity": 60, "liberty": 75 }},
  
+ //Liberty - Right-Libertarians
  { "name": "Right-Libertarianism",
  "link": "https://en.wikipedia.org/wiki/Right-libertarianism",
  "desc": "Right-Libertarianism is a political philosophy and movement that upholds liberty as a core principle. Right-Libertarians seek to maximize autonomy and political freedom, emphasizing free association, free markets, individualism and voluntary association.",
- "next": "text",
+ "next": "Bleeding Heart Libertarianism, Paleolibertarianism, Anarcho-Capitalism",
  "stats": { "care": 55, "fairness": 62, "loyalty": 43, "authority": 42, "sanctity": 45, "liberty": 85 }},
  
  { "name": "Bleeding-Heart Libertarianism",
  "link": "https://en.wikipedia.org/wiki/Neoclassical_liberalism",
  "desc": "Bleeding-Heart Libertarianism is a libertarian political philosophy that focuses on the support for civil liberties and free markets and the concern for social justice and the well-being of the disadvantaged.",
- "next": "text",
+ "next": "Right-Libertarianism, Paleolibertarianism, Anarcho-Capitalism",
  "stats": { "care": 65, "fairness": 62, "loyalty": 40, "authority": 42, "sanctity": 30, "liberty": 85 }},
  
  { "name": "Paleolibertarianism",
  "link": "https://en.wikipedia.org/wiki/Paleolibertarianism",
  "desc": "Paleolibertarianism is a variant of Libertarianism which stresses inherent incompatibility between progressivism and the concept of liberty, as well as a focus on the importance of inherited culture as a means of maintaining order.",
- "next": "text",
+ "next": "Right-Libertarianism, Bleeding Heart Libertarianism, Anarcho-Capitalism",
  "stats": { "care": 55, "fairness": 60, "loyalty": 60, "authority": 55, "sanctity": 60, "liberty": 80 }},
  
  { "name": "Anarcho-Capitalism",
  "link": "https://en.wikipedia.org/wiki/Anarcho-capitalism",
  "desc": "Anarcho-Capitalism is a political philosophy and economic theory that advocates the elimination of centralized states in favor of a system of private property enforced by private agencies, free markets and the right-libertarian interpretation of self-ownership, which extends the concept to include control of private property as part of the self.",
- "next": "text",
+ "next": "Anarcho-Capitalism, Paleolibertarianism, Bleeding Heart Libertarianism",
  "stats": { "care": 55, "fairness": 62, "loyalty": 43, "authority": 35, "sanctity": 45, "liberty": 100 }},
 
 ];

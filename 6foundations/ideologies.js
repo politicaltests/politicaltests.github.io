af8d1aa352e3bd67c005fcd7be39f2f9b89a96d1
politicaltests <75597907+politicaltests@users.noreply.github.com>
2021-08-25 21:52:22 +0800

    Update ideologies.js

--- 6foundations/ideologies.js
+++ 6foundations/ideologies.js
@@ -6,6 +6,32 @@ ideologies = [
  "next": "Social Democracy, Left-Wing Populism, Liberalism",
  "stats": { "care": 75, "fairness": 75, "loyalty": 42, "authority": 40, "sanctity": 25, "liberty": 62 }},
  
+ { "name": "Social Democracy",
+ "link": "https://en.wikipedia.org/wiki/Social_democracy",
+ "desc": "Social Democracy advocates for economic and social interventions to promote social justice within the framework of a liberal-democratic polity and a capitalist-oriented mixed economy.",
+ "next": "Democratic Socialism, Christian Democracy, Left-Wing Populism",
+ "stats": { "care": 75, "fairness": 85, "loyalty": 42, "authority": 45, "sanctity": 25, "liberty": 55 }},
+ 
+ { "name": "Christian Democracy",
+ "link": "https://en.wikipedia.org/wiki/Christian_democracy",
+ "desc": "Christian democracy is a political ideology based on modern democratic ideas, socialist ethics, and traditional Christian values. It tends to be Center-Left in terms of economic and foreign policy, but tends to be Center-Right in terms of social issues.",
+ "next": "Social Democracy, Progressive Conservatism, Left-Wing Populism",
+ "stats": { "care": 75, "fairness": 85, "loyalty": 42, "authority": 50, "sanctity": 70, "liberty": 50 }},
+ 
+ { "name": "Progressive Conservatism",
+ "link": "https://en.wikipedia.org/wiki/Progressive_conservatism",
+ "desc": "Progressive conservatism is a political ideology which combines conservative and progressive policies. It believes that social reform and progression in areas such as science, economics, education, technology and medicine were necessary to improve human living conditions, but also supports the preservation of tradition.",
+ "next": "Christian Democracy, Conservatism, Progressivism",
+ "stats": { "care": 70, "fairness": 75, "loyalty": 42, "authority": 50, "sanctity": 65, "liberty": 75 }},
+ 
+ //Fair - Socialists
+ { "name": "Democratic Socialism",
+ "link": "https://en.wikipedia.org/wiki/Democratic_socialism",
+ "desc": "Democratic socialism is a left-wing political philosophy that supports political democracy within a socially owned economy, with a particular emphasis on economic democracy, workplace democracy and workers' self-management within a market socialist economy, or an alternative form of a decentralised planned socialist economy.",
+ "next": "Social Democracy, Left-Wing Populism, Progressivism",
+ "stats": { "care": 75, "fairness": 85, "loyalty": 42, "authority": 45, "sanctity": 25, "liberty": 60 }},
+ 
+ //Sanctity - Conservatives
  { "name": "Conservatism",
  "link": "https://en.wikipedia.org/wiki/Conservatism",
  "desc": "Conservatism is a cultural, social, and political philosophy which seeks to promote and to preserve traditional social institutions. n Western culture, conservatives seek to preserve a range of institutions such as organized religion, parliamentary government, and property rights.",
